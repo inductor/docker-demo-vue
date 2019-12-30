2ea8757422562578ff68c2d8327dd84ec82c0df8
inductor <kohei.ota@zozo.com>
2019-12-30 22:51:20 +0900

    Add axios

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app">{{ info }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/")
+      .then(response => (this.info = response.data.message));
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
